c9b98d9abfdcc184d5dbacf9fed9b5da8a76f90b
RohitsSkill <73296362+RohitsSkill@users.noreply.github.com>
2020-11-20 10:36:18 +0530

    Update test.java

--- src/test.java
+++ src/test.java
@@ -4,11 +4,17 @@ import org.testng.annotations.Test;
 @Test
 public class test {
 	calculator calculator = new calculator();
-	
+
 	public void checkEmpty() {
 		assertEquals(calculator.add(""),0);
 	}
 	public void checksingle() {
 		assertEquals(calculator.add("1"),1);
+	}
+	public void checksum() {
+		assertEquals(calculator.add("1,3"),4);
+	}
+	public void checkMultiple() {
+		assertEquals(calculator.add("1,3,2"),6);
 	}
 }
